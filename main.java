ffeeca3077779ce57fcfcd8161c09704d18a8f16
Adelllin <138500349+Adelllin@users.noreply.github.com>
2024-04-03 19:19:32 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,40 @@
+//OR
+//I will buy eggs if they will be on the sale OR they will cost less than 2.50
+//I will go running, if I will have enough energy OR if the weather is shiny.
+public class Main {
+  public static void main(String[] args) {
+   //winter, spring, summer, autumn
+    //warm jacket, swimming suite, rain coat, sweater
+
+    //Temperature
+   // +5 wear super warm
+  //  +5 to +15 warm
+   // +15 to +30 normal
+   // +30 and more you need cooling
+    
+    var temp = 15.89;
+    
+    if (temp <+5){
+      System.out.println("Wear super warm");
+    }
+    else if (temp <=15){
+      
+        System.out.println("Wear  warm");
+    }
+      else if (temp <=30){
+
+      System.out.println("Wear normal");
+  }
+  else if (temp >= +30)
+  
+  System.out.println("Wear cooling");
+  
+  }
+}
+
+      
+    
+
+
+
+
